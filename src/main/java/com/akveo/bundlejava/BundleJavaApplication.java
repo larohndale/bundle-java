c8735c0c8d370be77b42899785015b80e40d2355
Eugen Sadovsky <e.sadovsky@akveo.com>
2019-04-23 15:59:32 +0300

    feat(structure): initial commit

---
+++ src/main/java/com/akveo/bundlejava/BundleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.akveo.bundlejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundleJavaApplication.class, args);
+	}
+
+}
