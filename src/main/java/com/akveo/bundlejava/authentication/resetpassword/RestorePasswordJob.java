4dbdff963b58b6f3f44bf8519e0018067268965e
Eugen Sadovsky <e.sadovsky@akveo.com>
2019-05-01 00:07:26 +0300

    feat(props): change secret keys

--- src/main/java/com/akveo/bundlejava/authentication/resetpassword/RestorePasswordJob.java
+++ src/main/java/com/akveo/bundlejava/authentication/resetpassword/RestorePasswordJob.java
@@ -9,7 +9,6 @@ package com.akveo.bundlejava.authentication.resetpassword;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
