89530403f4cae27a831a71af48d272e788308a67
Eugen Sadovsky <e.sadovsky@akveo.com>
2019-04-30 11:56:00 +0300

    feat(app): add copyright info

--- src/main/java/com/akveo/bundlejava/authentication/AuthService.java
+++ src/main/java/com/akveo/bundlejava/authentication/AuthService.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Licensed under the Personal / Commercial License.
+ * See LICENSE_PERSONAL / LICENSE_COMMERCIAL in the project root for license information on type of purchased license.
+ */
+
 package com.akveo.bundlejava.authentication;
 
 import com.akveo.bundlejava.authentication.exception.InvalidTokenHttpException;
--- src/main/java/com/akveo/bundlejava/authentication/resetpassword/RestorePassword.java
+++ src/main/java/com/akveo/bundlejava/authentication/resetpassword/RestorePassword.java
@@ -1,5 +1,10 @@
-package com.akveo.bundlejava.authentication.resetpassword;
+/*
+ * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Licensed under the Personal / Commercial License.
+ * See LICENSE_PERSONAL / LICENSE_COMMERCIAL in the project root for license information on type of purchased license.
+ */
 
+package com.akveo.bundlejava.authentication.resetpassword;
 
 import com.akveo.bundlejava.user.User;
 
