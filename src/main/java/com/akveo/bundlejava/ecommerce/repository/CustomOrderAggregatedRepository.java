5584b818a2c55306b7fa968fe40a25dfd07a23be
Sergey Veselov <s.veselov@akveo.com>
2019-09-09 14:24:56 +0300

    select year with count

--- src/main/java/com/akveo/bundlejava/ecommerce/repository/CustomOrderAggregatedRepository.java
+++ src/main/java/com/akveo/bundlejava/ecommerce/repository/CustomOrderAggregatedRepository.java
@@ -10,8 +10,11 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Tuple;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 import java.lang.reflect.Type;
 import java.sql.Timestamp;
@@ -32,18 +35,12 @@ public class CustomOrderAggregatedRepository {
         Session session = em.unwrap(Session.class);
         CriteriaBuilder builder = session.getCriteriaBuilder();
 
-        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-        CriteriaQuery<Order> criteriaQuery = builder.createQuery(Order.class);
+        CriteriaQuery<Tuple> criteriaQuery = builder.createQuery(Tuple.class);
         Root<Order> order = criteriaQuery.from(Order.class);
-        //criteriaQuery.select(order , criteriaBuilder.count(order)).groupBy(order.get("date").get("year"));
-        criteriaQuery.select(order.get("date"));//в этой строчке нужно сделать группировку по годам(или месяцам)
-
-//        List result = session.createCriteria(Order.class)
-//                .add(Restrictions.between("date",  LocalDateTime.now().minusYears(4), LocalDateTime.now()))
-//                .setProjection(Projections.groupProperty("date")).list();
-//setProjection(Projections.sqlGroupProjection("year(date)", "year(date)",new String[]{"XXX"}, new Type[]{Hibernate.DATE})).list();
-
-        List<Order> results = em.createQuery(criteriaQuery).getResultList();
+        Expression yearFunction = builder.function("year", Integer.class, order.get("date"));
+        criteriaQuery.groupBy(yearFunction);
+        criteriaQuery.multiselect(yearFunction, builder.count(order.get("date")));//в этой строчке нужно сделать группировку по годам(или месяцам)
+        List<Tuple> results = em.createQuery(criteriaQuery).getResultList();
        return null;
     }
 }
